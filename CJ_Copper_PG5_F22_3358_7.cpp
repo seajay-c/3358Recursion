3994f11d1823bfcd14965e912e632172f94c6b68
sea jay <70167969+seajay-c@users.noreply.github.com>
2022-10-24 16:18:01 -0500

    Update CJ_Copper_PG5_F22_3358_7.cpp

--- CJ_Copper_PG5_F22_3358_7.cpp
+++ CJ_Copper_PG5_F22_3358_7.cpp
@@ -1,1 +1,18 @@
-
+// Author : CJ COPPER
+//
+// Serial Number : 8
+//
+// Due Date : 10/31/22
+// Programming Assignment Number 5
+//
+// Fall 2022 - CS 3358 - 7
+//
+// Instructor: Husain Gholoom.
+//
+// <Brief description of the purpose of the program>
+#include <iostream>
+using namespace std;
+int main(){
+  cout<<"six"<<endl;
+  return 0;
+}
